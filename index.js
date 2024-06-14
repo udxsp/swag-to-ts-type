e992708dd34a86e7c897552feb9c82101d77e3bc
Luca-Pulina <luca.pulina@alpitourworld.it>
2024-06-14 15:12:27 +0200

    feat: better converter

--- index.js
+++ index.js
@@ -3,28 +3,33 @@ import axios from 'axios';
 
 function toPascalCase(str) {
     return str
-        // Prima, sostituisci ogni '/' con uno spazio per identificare le parole
         .replace(/\//g, ' ')
-        // Poi, converti ogni parola in PascalCase
         .replace(/(?:^\w|[A-Z]|\b\w)/g, function(match, index) {
             return match.toUpperCase();
         })
-        // Infine, rimuovi i caratteri '-' e '_' e converti il carattere successivo in maiuscolo
         .replace(/[-_]\w/g, function(match) {
             return match.charAt(1).toUpperCase();
         })
         .replace(/\s+/g, '');
 }
-  
+
+// Funzione per risolvere i riferimenti $ref
+function resolveRef(ref, definitions) {
+    const refPath = ref.split('/');
+    return definitions[refPath[refPath.length - 1]];
+}
 
 // Funzione per generare i tipi TypeScript dagli schemi Swagger
-function generateTypes(schema) {
-  if (schema.type === 'object') {
+function generateTypes(schema, definitions, typeName) {
+  if (schema.$ref) {
+    const refSchema = resolveRef(schema.$ref, definitions);
+    return refSchema ? refSchema.title || schema.$ref.split('/').pop() : 'any';
+  } else if (schema.type === 'object') {
     return `{ ${Object.keys(schema.properties)
-      .map((key) => `${key}: ${generateTypes(schema.properties[key])}`)
+      .map((key) => `${key}: ${generateTypes(schema.properties[key], definitions)}`)
       .join('; ')} }`;
   } else if (schema.type === 'array') {
-    return `${generateTypes(schema.items)}[]`;
+    return `${generateTypes(schema.items, definitions)}[]`;
   } else if (schema.type === 'integer') {
     return 'number';
   } else {
@@ -36,20 +41,17 @@ function generateTypes(schema) {
 async function convertSwaggerToTS(swaggerUrl, outputFilePath) {
   try {
     const response = await axios.get(swaggerUrl);
-    // Legge e parse il file Swagger
-
     const swaggerJSON = response.data;
     let tsContent = '';
-    
 
     // Genera tipi per le definizioni
-    const definitions = {};
+    const definitions = swaggerJSON.definitions || {};
     if (swaggerJSON.definitions) {
       tsContent += '/* Definitions */\n';
       for (const defName in swaggerJSON.definitions) {
         const def = swaggerJSON.definitions[defName];
-        const type = generateTypes(def);
-        definitions[defName] = defName;
+        const type = generateTypes(def, definitions, defName);
+        definitions[defName] = type;
         tsContent += `export type ${defName} = ${type};\n\n`;
       }
     }
@@ -60,7 +62,6 @@ async function convertSwaggerToTS(swaggerUrl, outputFilePath) {
       for (const path in swaggerJSON.paths) {
         for (const method in swaggerJSON.paths[path]) {
           const operation = swaggerJSON.paths[path][method];
-          // Rimuove il primo slash, converte in PascalCase e rimuove i caratteri '{}'
           const endpointName = toPascalCase(path.slice(1).replace(/[{}]/g, ''));
           const requestTypeName = `${endpointName}Req`;
           const responseTypeName = `${endpointName}Res`;
@@ -70,7 +71,7 @@ async function convertSwaggerToTS(swaggerUrl, outputFilePath) {
           if (operation.parameters) {
             const params = operation.parameters.reduce((acc, param) => {
               const paramType = param.schema
-                ? (param.schema.$ref ? definitions[param.schema.$ref.split('/').pop()] : generateTypes(param.schema))
+                ? (param.schema.$ref ? generateTypes({ $ref: param.schema.$ref }, definitions) : generateTypes(param.schema, definitions))
                 : (param.type === 'integer' ? 'number' : param.type || 'any');
               acc[param.name] = paramType;
               return acc;
@@ -84,8 +85,8 @@ async function convertSwaggerToTS(swaggerUrl, outputFilePath) {
             const responseSchema = operation.responses['200'].schema;
             if (responseSchema) {
               responseType = responseSchema.$ref
-                ? definitions[responseSchema.$ref.split('/').pop()]
-                : generateTypes(responseSchema);
+                ? generateTypes({ $ref: responseSchema.$ref }, definitions)
+                : generateTypes(responseSchema, definitions);
             }
           }
 
