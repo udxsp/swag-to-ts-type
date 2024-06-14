b3a4d1bda085f013c73015193afe9e493622b216
udxsp <udxsp@outlook.com>
2024-06-14 18:07:51 +0200

    fix: generate command

--- bin/generate.js
+++ bin/generate.js
@@ -2,4 +2,17 @@
 
 import { execSync } from "child_process"
 
-execSync("npm run generate", { stdio: "inherit" })
+const args = process.argv.slice(2) // Get the command line arguments
+const linkFileFrom = args[0] // Assume the parameter is the first argument
+
+if (!linkFileFrom) {
+	console.error("Error: link_file_from parameter is required.")
+	process.exit(1)
+}
+
+try {
+	execSync(`npm run generate -- ${linkFileFrom}`, { stdio: "inherit" })
+} catch (error) {
+	console.error('Error running "npm run generate":', error)
+	process.exit(1) // Exit with error code
+}
